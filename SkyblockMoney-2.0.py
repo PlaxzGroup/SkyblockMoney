e51d86767b309e174ae5ade04efac9512662ee15
la_you <1665571328@qq.com>
2020-10-06 21:51:52 +0800

    Formatting code and some optimizations

--- SkyblockMoney-2.0.py
+++ SkyblockMoney-2.0.py
@@ -1,32 +1,33 @@
 from urllib import request, error
-money= 0
-name= input('Enter Player ID:')
-url= "http://sky.shiiyu.moe/api/v2/coins/"+name
-headers= {'User-Agent':''}
+
+money = 0
+name = input('Enter Player ID:')
+url = "http://sky.shiiyu.moe/api/v2/coins/" + name
+headers = {'User-Agent': ''}
 try:
-    response= request.urlopen(request.Request(url,headers=headers)).read()
+    response = request.urlopen(request.Request(url, headers=headers)).read().decode('utf-8')
 except error.HTTPError as e:
-    if e.code==500:
+    if e.code == 500:
         print('Wrong player ID or this player has no profile.')
     else:
         print('Wrong connection, Please retry.')
 else:
-    #print(response)
-    finding= ['purse','bank']
-    adding=[7,6]
+    # print(response)
+    finding = ['purse', 'bank']
+    adding = [7, 6]
     for j in range(len(finding)):
-        temp= response.decode('utf-8')
+        temp = response
         while True:
             try:
-                temp= temp[temp.index(finding[j])+adding[j]:]
+                temp = temp[temp.index(finding[j]) + adding[j]:]
             except ValueError:
                 break
             else:
-                _money= 0
+                _money = 0
                 for i in range(len(temp)):
-                    if temp[i]!=',' and temp[i]!='.' and temp[i]!='}':
-                        _money= _money*10+int(temp[i])
+                    if temp[i] != ',' and temp[i] != '.' and temp[i] != '}':
+                        _money = _money * 10 + int(temp[i])
                     else:
                         break
-                money+=_money
-    print(name,' has ',int(money),' coins in Skyblock.')
+                money += _money
+    print(name, ' has ', int(money), ' coins in Skyblock.')
